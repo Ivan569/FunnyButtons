74dc81b6db67dc9e5b56ab9eb62da8f884cfad4c
IvanPlatonov <58946483+Ivan569@users.noreply.github.com>
2021-02-23 14:21:14 +0300

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿namespace FunnyButtons
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Record = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.Time = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(40, 20);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(149, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(40, 20);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "2";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.Button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Location = new System.Drawing.Point(12, 129);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(40, 20);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.Button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button4.Location = new System.Drawing.Point(149, 129);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(40, 20);
+            this.button4.TabIndex = 3;
+            this.button4.Text = "4";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.Button4_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-15, 167);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 4;
+            // 
+            // Record
+            // 
+            this.Record.AutoSize = true;
+            this.Record.Location = new System.Drawing.Point(0, 172);
+            this.Record.Name = "Record";
+            this.Record.Size = new System.Drawing.Size(48, 13);
+            this.Record.TabIndex = 5;
+            this.Record.Text = "Record: ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(81, 172);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Now: ";
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Time
+            // 
+            this.Time.AutoSize = true;
+            this.Time.Location = new System.Drawing.Point(146, 172);
+            this.Time.Name = "Time";
+            this.Time.Size = new System.Drawing.Size(42, 13);
+            this.Time.TabIndex = 7;
+            this.Time.Text = "Time: 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(201, 189);
+            this.Controls.Add(this.Time);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Record);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Funny buttons";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Record;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label Time;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Reflection;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Windows.Forms;
+
+namespace FunnyButtons
+{
+    public partial class Form1 : Form
+    {
+        private int timerCounter = 0;
+        private int nowCount = 0;
+        private int recordCount = 0;
+        private const string fileName = "record";
+
+        public Form1() => InitializeComponent();
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            for (int i = 0; i < Width / 2; i++)
+                label1.Text += "―";
+            timer1.Start();
+            timer1.Interval = 1000;
+            Deserialize();
+            Record.Text = "Record: " + recordCount;
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            nowCount++;
+            ChangeLocationOfButtons();
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            nowCount += 2;
+            ChangeLocationOfButtons();
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            nowCount += 3;
+            ChangeLocationOfButtons();
+        }
+
+        private void Button4_Click(object sender, EventArgs e)
+        {
+            nowCount += 4;
+            ChangeLocationOfButtons();
+        }
+
+        private void ChangeLocationOfButtons()
+        {
+            Random rnd = new Random();
+            button1.Location = new Point(rnd.Next(0, Width - button1.Width * 3), rnd.Next(0, Height - button1.Height * 4 - 7));
+            button2.Location = new Point(rnd.Next(0, Width - button2.Width * 3), rnd.Next(0, Height - button2.Height * 4 - 7));
+            button3.Location = new Point(rnd.Next(0, Width - button3.Width * 3), rnd.Next(0, Height - button3.Height * 4 - 7));
+            button4.Location = new Point(rnd.Next(0, Width - button4.Width * 3), rnd.Next(0, Height - button4.Height * 4 - 7));           
+            label2.Text = "Now: " + nowCount;
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            Time.Text = "Time: " + (++timerCounter).ToString();
+            if (timerCounter == 60)
+            {
+                timer1.Stop();
+                MessageBox.Show("Well done! Your account: " + nowCount + "!", "The outcome of the game");
+                if (recordCount < nowCount)
+                    Serialize(nowCount);
+                Process.Start(Assembly.GetEntryAssembly().Location);
+                Process.GetCurrentProcess().Kill();
+            }
+        }       
+
+        private void Serialize(int count)
+        {
+            FileStream fs = new FileStream(fileName + ".s", FileMode.Create, FileAccess.Write, FileShare.ReadWrite);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, count);
+            fs.Close();
+        }
+
+        private void Deserialize()
+        {
+            try
+            {
+                FileStream fs = new FileStream(fileName + ".s", FileMode.Open, FileAccess.Read, FileShare.Read);
+                BinaryFormatter bf = new BinaryFormatter();
+                recordCount = (int)bf.Deserialize(fs);
+                fs.Close();
+            }
+            catch { }
+        }               
+    }
+}
